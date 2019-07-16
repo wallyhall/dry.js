d96ff30a08cbec14d76401965a48aa7b81004ac5
Matthew Hall <matt@fidelityworks.co.uk>
2019-07-17 00:19:27 +0100

    Removed unused goodbye widget from calc demo

--- demo/calc/widgets/goodbye/main.js
+++
@@ -1,6 +0,0 @@
-dry.registerWidget('goodbye', [], function() {
-    this.init = function(args) {
-    console.log("hi", args);
-        this.dom.html(args.msg + '<br>Init ran at ' + Date());
-    };
-});
